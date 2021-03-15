b5cb5aa8873f02f7c891bc8ca9a0cf6c7085154e
nueltogo <nunana.togo@turntabl.io>
2021-03-15 12:39:14 +0000

    Initial Commit

---
+++ src/main/java/com/example/ordervalidationservice/OrderValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordervalidationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderValidationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ordervalidationservice/OrderValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordervalidationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
